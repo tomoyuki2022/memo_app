3b0e9a6cd7874fc71e7ed66d3450348e934f3a18
tomoyuki <at01110833@gmail.com>
2023-11-16 17:07:19 +0900

    課題修正

--- memoapp.rb
+++ memoapp.rb
@@ -14,6 +14,12 @@ def col_memo_id
   memo_id + 1
 end
 
+def write_json_file(file_data)
+  File.open('public/memos.json', 'w') do |file|
+    file.write(JSON.pretty_generate(file_data))
+  end
+end
+
 not_found do
   erb :not_found
 end
@@ -36,41 +42,35 @@ end
 get '/memos/:memo_id' do
   json_data = read_json_file
   memo_id = params[:memo_id].to_i
-  pass unless json_data.find { |data| data['id'] == memo_id }
-  @finded_memo = json_data.find { |data| data['id'] == memo_id }
+  @found_memo = json_data.find { |data| data['id'] == memo_id }
+  pass unless @found_memo
   erb :show_memo
 end
 
 post '/memos' do
   memo_id = col_memo_id
   json_data = read_json_file
-  input_title = params[:title] == '' ? params[:title] = '未入力' : params[:title]
-  new_memo = { 'id' => memo_id, 'title' => h(input_title), 'content' => h(params[:content]) }
+  new_memo = { 'id' => memo_id, 'title' => params[:title], 'content' => params[:content] }
   json_data << new_memo
-  File.open('public/memos.json', 'w') do |file|
-    file.write(JSON.pretty_generate(json_data))
-  end
+  write_json_file(json_data)
   redirect '/'
 end
 
 get '/edit_memos/:edit_memo_id' do
   json_data = read_json_file
   memo_id = params[:edit_memo_id].to_i
-  pass unless json_data.find { |data| data['id'] == memo_id }
-  @finded_memo = json_data.find { |data| data['id'] == memo_id }
+  @found_memo = json_data.find { |data| data['id'] == memo_id }
+  pass unless @found_memo
   erb :edit_memo
 end
 
-patch '/edit_memos/:edit_memo_id' do
-  memo_id = params[:edit_memo_id].to_i
+patch '/memos/:memo_id' do
+  memo_id = params[:memo_id].to_i
   json_data = read_json_file
-  finded_memo = json_data.find { |data| data['id'] == memo_id }
-  input_title = params[:title] == '' ? params[:title] = '未入力' : params[:title]
-  finded_memo['title'] = h(input_title)
-  finded_memo['content'] = h(params[:content])
-  File.open('public/memos.json', 'w') do |file|
-    file.write(JSON.pretty_generate(json_data))
-  end
+  found_memo = json_data.find { |data| data['id'] == memo_id }
+  found_memo['title'] = params[:title]
+  found_memo['content'] = params[:content]
+  write_json_file(json_data)
   redirect '/'
 end
 
@@ -78,8 +78,6 @@ delete '/memos/:memo_id' do
   memo_id = params[:memo_id].to_i
   json_data = read_json_file
   json_data.delete_if { |data| data['id'] == memo_id }
-  File.open('public/memos.json', 'w') do |file|
-    file.write(JSON.pretty_generate(json_data))
-  end
+  write_json_file(json_data)
   redirect '/'
 end
